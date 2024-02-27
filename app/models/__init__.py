2267a0c61a862b9755fbdc1407e32ae9c616702d
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-02-27 14:49:33 -0500

    Add User model and add additional validation in supplied user auth files.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,2 @@
-from .db import db
+from .db import db, environment, SCHEMA
 from .user import User
-from .db import environment, SCHEMA
--- app/models/user.py
+++ app/models/user.py
@@ -1,18 +1,71 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy.orm import validates
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
+        __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
-    hashed_password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    hashed_password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    profile_image_url = db.Column(db.String)
+    is_deleted = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    # //*====> One-to-Many <====
+    workout_plans = db.relationship("WorkoutPlan", back_populates="user")
+    exercises = db.relationship(
+        "Exercise", back_populates="user", cascade="all, delete"
+    )
+    nutrition_profiles = db.relationship("NutritionProfile", back_populates="user")
+    body_measurements = db.relationship("BodyMeasurement", back_populates="user")
+
+    # //*====> Many-to-Many <====
+    # insert association table here if needed later
+
+    @validates("first_name")
+    def validate_first_name(self, _, val):
+        if not len(val):
+            raise ValueError({"first_name": "First name is required"})
+        return val
+
+    @validates("last_name")
+    def validate_last_name(self, _, val):
+        if not len(val):
+            raise ValueError({"last_name": "Last name is required"})
+        return val
+
+    @validates("username")
+    def validate_username(self, _, val):
+        if len(val) < 4:
+            raise ValueError(
+                {"username": "Username must be at least 4 characters long"}
+            )
+        if len([user for user in User.query.all() if user.username == val]):
+            raise ValueError({"username": "User with that username already exists"})
+        return val
+
+    @validates("email")
+    def validate_email(self, _, val):
+        if "@" not in val:
+            raise ValueError({"message": "Invalid email"})
+        if len([user for user in User.query.all() if user.email == val]):
+            raise ValueError({"email": "Email is already in use"})
+        return val
+
+    @classmethod
+    def username_to_ids(cls):
+        return {user.username: user.id for user in cls.query.all()}
 
     @property
     def password(self):
@@ -27,7 +80,11 @@ class User(db.Model, UserMixin):
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "username": self.username,
+            "email": self.email,
+            "profile_image_url": self.profile_image_url,
+            "is_deleted": self.is_deleted,
         }
