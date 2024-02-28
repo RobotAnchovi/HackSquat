5b8b939d5c059a565095628ab9cd2a45651ae5a2
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-02-28 00:06:38 -0500

    Add SQLAlchemy imports and update connection handling

--- migrations/env.py
+++ migrations/env.py
@@ -8,6 +8,9 @@ from flask import current_app
 from alembic import context
 
 import os
+from sqlalchemy import engine_from_config
+from typing import cast
+from sqlalchemy.engine import Connection
 
 environment = os.getenv("FLASK_ENV")
 SCHEMA = os.environ.get("SCHEMA")
@@ -87,28 +90,30 @@ def run_migrations_online():
                 directives[:] = []
                 logger.info("No changes in schema detected.")
 
-    connectable = engine_from_config(  # type: ignore
+    connectable = engine_from_config(
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,  # type: ignore
     )
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,  # type: ignore
-            process_revision_directives=process_revision_directives,
-            **current_app.extensions["migrate"].configure_args,
-        )
-        # Create a schema (only in production)
-        if environment == "production":
-            connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
+    # ...
+
+    connection = connectable.connect()
+    context.configure(
+        connection=cast(Connection, connection),
+        target_metadata=target_metadata,  # type: ignore
+        process_revision_directives=process_revision_directives,
+        **current_app.extensions["migrate"].configure_args,
+    )
+    # Create a schema (only in production)
+    if environment == "production":
+        connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
 
         # Set search path to your schema (only in production)
-        with context.begin_transaction():
-            if environment == "production":
-                context.execute(f"SET search_path TO {SCHEMA}")
-            context.run_migrations()
+    with context.begin_transaction():
+        if environment == "production":
+            context.execute(f"SET search_path TO {SCHEMA}")
+        context.run_migrations()
 
 
 if context.is_offline_mode():
