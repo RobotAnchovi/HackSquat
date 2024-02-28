62d8f5dec8929c337da27a065203ed8cb6ee1999
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-02-27 22:34:51 -0500

    Update down_revision in merge_heads.py

--- migrations/versions/ddaca65062aa_merge_heads.py
+++ migrations/versions/ddaca65062aa_merge_heads.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "ddaca65062aa"
-down_revision = "ac6d37426d15"
+down_revision = "ab7d5f2df2f2"
 branch_labels = None
 depends_on = None
 
