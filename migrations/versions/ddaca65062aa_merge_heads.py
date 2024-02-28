d8ad779c06b856734620ac211ea3803ffac0ef25
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-02-27 22:22:15 -0500

    Add merge heads migration file

---
+++ migrations/versions/ddaca65062aa_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: ddaca65062aa
+Revises: 
+Create Date: 2024-02-27 22:11:37.766411
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ddaca65062aa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
