6b41674b3c468562aaf0702ba3806b93226f5cbf
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-02-29 18:14:48 -0500

    Initial API routes completed for workout_exercises and for workouts. Generating seed data for testing. 😎

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import generate_csrf
 from flask_login import LoginManager
-from .models import db, User, WorkoutPlan, Exercise
+from .models import db, User, WorkoutPlan, Exercise, WorkoutExercise, Workout
 from .api.workout_plan_routes import workout_plan_routes
 from .api.exercise_routes import exercise_routes
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
+from .api.workout_exercises_routes import workout_exercises_routes
+from .api.workout_routes import workout_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -32,6 +34,9 @@ app.register_blueprint(auth_routes, url_prefix="/api/auth")
 app.register_blueprint(user_routes, url_prefix="/api/users")
 app.register_blueprint(workout_plan_routes, url_prefix="/api/workout-plans")
 app.register_blueprint(exercise_routes, url_prefix="/api/exercises")
+app.register_blueprint(workout_exercises_routes, url_prefix="/api/workout-exercises")
+app.register_blueprint(workout_routes, url_prefix="/api/workouts")
+
 db.init_app(app)
 Migrate(app, db)
 
---
+++ app/api/workout_routes.py
@@ -0,0 +1,101 @@
+from flask import Blueprint, request, jsonify
+from flask_login import login_required, current_user
+from app.models import db, Workout
+from app.forms import WorkoutForm
+from sqlalchemy.exc import IntegrityError, DataError
+
+workout_routes = Blueprint("workouts", __name__)
+
+
+# //*====> CREATE a new workout <====
+@workout_routes.route("/", methods=["POST"])
+@login_required
+def create_workout():
+    form = WorkoutForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+    if form.validate():
+        try:
+            workout = Workout(  # type: ignore
+                workout_plan_id=form.data.get("workout_plan_id"),
+                user_id=current_user.id,
+                date=form.data["date"],
+                duration=form.data.get("duration"),
+                intensity=form.data.get("intensity"),
+                location=form.data.get("location"),
+                status=form.data["status"],
+                notes=form.data.get("notes"),
+            )
+            db.session.add(workout)
+            db.session.commit()
+            return jsonify(workout.to_dict()), 201
+        except IntegrityError:
+            db.session.rollback()
+            return jsonify({"errors": "Workout already exists"}), 400
+        except DataError:
+            db.session.rollback()
+            return jsonify({"errors": "Invalid data, check field formats."}), 400
+    return jsonify({"errors": form.errors}), 400
+
+
+# //*====> GET Workouts for a User <====
+@workout_routes.route("/user/<int:user_id>", methods=["GET"])
+@login_required
+def get_workouts(user_id):
+    if user_id != current_user.id:
+        return jsonify({"errors": "Unauthorized"}), 401
+    workouts = Workout.query.filter_by(user_id=user_id).all()
+    return jsonify([workout.to_dict() for workout in workouts]), 200
+
+
+# //*====> GET a single workout <====
+@workout_routes.route("/<int:workout_id>", methods=["GET"])
+@login_required
+def get_workout_details(workout_id):
+    workout = Workout.query.get(workout_id)
+    if workout and workout.user_id == current_user.id:
+        return jsonify(workout.to_dict()), 200
+    return jsonify({"errors": "Workout not found or unauthorized"}), 404
+
+
+# //*====> UPDATE a workout <====
+@workout_routes.route("/<int:workout_id>", methods=["PUT"])
+@login_required
+def update_workout(workout_id):
+    workout = Workout.query.get(workout_id)
+    if not workout:
+        return jsonify({"errors": "Workout not found."}), 404
+    if workout.user_id != current_user.id:
+        return jsonify({"errors": "Unauthorized"}), 403
+
+    form = WorkoutForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+    if form.validate():
+        try:
+            workout.workout_plan_id = form.data.get("workout_plan_id")
+            workout.date = form.data["date"]
+            workout.duration = form.data.get("duration")
+            workout.intensity = form.data.get("intensity")
+            workout.location = form.data.get("location")
+            workout.status = form.data["status"]
+            workout.notes = form.data.get("notes")
+            db.session.commit()
+            return jsonify(workout.to_dict()), 200
+        except IntegrityError:
+            db.session.rollback()
+            return jsonify({"errors": "Workout already exists"}), 400
+        except DataError:
+            db.session.rollback()
+            return jsonify({"errors": "Invalid data, check field formats."}), 400
+    return jsonify({"errors": form.errors}), 400
+
+
+# //*====> DELETE a workout <====
+@workout_routes.route("/<int:workout_id>", methods=["DELETE"])
+@login_required
+def delete_workout(workout_id):
+    workout = Workout.query.get(workout_id)
+    if workout and workout.user_id == current_user.id:
+        db.session.delete(workout)
+        db.session.commit()
+        return jsonify({"message": "Workout successfully deleted."}), 200
+    return jsonify({"errors": "Workout not found or unauthorized"}), 404
