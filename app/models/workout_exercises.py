ccf3502a41967674a2134e8dbe267b7416d1547f
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-03-01 17:51:32 -0500

    Fix exercise_id foreign key reference

--- app/models/workout_exercises.py
+++ app/models/workout_exercises.py
@@ -16,7 +16,7 @@ class WorkoutExercise(db.Model):
         nullable=False,
     )
     exercise_id = db.Column(
-        db.Integer, db.ForeignKey("exercises.exercise_id"), nullable=False
+        db.Integer, db.ForeignKey(f"{('exercises')}.exercise_id"), nullable=False
     )
 
     # ^ Fields for weight-based exercises
