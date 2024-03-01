dc46677657e9fe42eefe8c78a04ab96e0d0ff70e
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-03-01 14:47:28 -0500

    Fix logging configuration and create schema in production

--- migrations/env.py
+++ migrations/env.py
@@ -20,7 +20,7 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)  # type: ignore
+fileConfig(config.config_file_name)
 logger = logging.getLogger("alembic.env")
 
 # add your model's MetaData object here
@@ -74,7 +74,7 @@ def run_migrations_online():
                 logger.info("No changes in schema detected.")
 
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),  # type: ignore
+        config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
@@ -88,7 +88,7 @@ def run_migrations_online():
         )
         # Create a schema (only in production)
         if environment == "production":
-            connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")  # type: ignore
+            connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
 
         # Set search path to your schema (only in production)
         with context.begin_transaction():
